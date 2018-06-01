bb7867ec04b7672d0b1213b3889b9a3cac7af3c7
Chris Korbel <ckorbel1@gmail.com>
2018-06-01 07:45:41 -0700

    User model bcrypt set up

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,48 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+//Load User Model
+
+const User = require("../../models/User");
 
 //app.use("/api/users", users); the , users tells the route to go here and get /test
 //@route GET request to api/post/test
 //@description Tests post route
 //@access public route
 router.get("/test", (req, res) => res.json({ message: "Users works" }));
+
+router.post("/register", (req, res) => {
+  //first check is user already exits
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "email already exits" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", //size
+        r: "pg", //rating
+        d: "mm" //default
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar: avatar, //since these are the same es6 lets you do just avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 
 module.exports = router;
