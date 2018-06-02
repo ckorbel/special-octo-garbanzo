3c754d6f0b091017896ed9eb37a3a0e9a5e57821
Chris Korbel <ckorbel1@gmail.com>
2018-06-01 18:08:44 -0700

    server side validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -24,13 +24,15 @@ router.get("/test", (req, res) => res.json({ message: "Users works" }));
 
 router.post("/register", (req, res) => {
   const { errors, isValid } = validateRegisterInput(req.body);
+
   if (!isValid) {
     return res.status(400).json(errors);
   }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      console.log(req);
-      return res.status(400).json({ email: "email already exits" });
+      errors.email = "Email already exists";
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", //size
--- validation/is-empty.js
+++ validation/is-empty.js
@@ -1,12 +1,9 @@
 //this function is used so that we can check all data types validator only check for strings
 
-function isEmpty(value) {
-  return (
-    value === undefined ||
-    value === null ||
-    (typeof value === "object" && Object.length.keys(value).length === 0) ||
-    (typeof value === "string" && value.trim().length === 0)
-  );
-}
+const isEmpty = value =>
+  value === undefined ||
+  value === null ||
+  (typeof value === "object" && Object.keys(value).length === 0) ||
+  (typeof value === "string" && value.trim().length === 0);
 
 module.exports = isEmpty;
