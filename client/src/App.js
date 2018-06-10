802f83c2c9a8afffed0a7265df14a5d4588e2bbf
Chris Korbel <ckorbel1@gmail.com>
2018-06-10 00:02:02 -0700

    added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
 
 import "./App.css";
@@ -8,11 +13,17 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <h1>Hello world</h1>
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
