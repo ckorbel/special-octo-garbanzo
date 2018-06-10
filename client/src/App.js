2b658996f2d259d5e60e7d6d887b092d1b9180ab
Chris Korbel <ckorbel1@gmail.com>
2018-06-09 17:09:09 -0700

    starting to add component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+
+import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/Navbar";
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <h1>Hello world</h1>
+        <Footer />
       </div>
     );
   }
