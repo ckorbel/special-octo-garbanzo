925a56785746a6927adb2cac13632366f7142ad1
Chris Korbel <ckorbel1@gmail.com>
2018-06-17 23:38:51 -0700

    create profile added to redux

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -25,6 +25,20 @@ export const getCurrentProfile = () => dispatch => {
     );
 };
 
+//Create Profile
+
+export const createProfile = (profileData, history) => dispatch => {
+  axios
+    .post("/api/profile", profileData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 export const setProfileLoading = () => {
   return {
     type: PROFILE_LOADING
