7c756fb53127bb3ba0bf1345a0a5f0fa2eea9e40
Chris Korbel <ckorbel1@gmail.com>
2018-06-19 21:49:43 -0700

    add experience created

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/create-profile/CreateProfile";
 import EditProfile from "./components/edit-profile/EditProfile";
-
+import AddExperience from "./components/add-credentials/AddExperience";
 import "./App.css";
 
 //ensures that a page refresh doesn't get rid of user authorization
@@ -62,6 +62,13 @@ class App extends Component {
                   exact
                   path="/edit-profile"
                   component={EditProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-experience"
+                  component={AddExperience}
                 />
               </Switch>
             </div>
