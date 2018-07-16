cec4b4fa774a5981e5650b4c678d57d95b5479bd
Chris Korbel <ckorbel1@gmail.com>
2018-07-15 18:20:15 -0700

    view profile by handle

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,8 @@ import CreateProfile from "./components/create-profile/CreateProfile";
 import EditProfile from "./components/edit-profile/EditProfile";
 import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
-import Profiles from './components/profile/Profiles';
+import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
 import "./App.css";
 
 //ensures that a page refresh doesn't get rid of user authorization
@@ -50,6 +51,7 @@ class App extends Component {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:handle" component={Profile} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
               </Switch>
