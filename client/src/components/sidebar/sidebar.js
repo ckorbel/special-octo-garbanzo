a385b6217ea4661b0e244597e31c7f2c926046f0
Chris Korbel <ckorbel1@gmail.com>
2018-09-03 09:40:04 -0700

    adding more components

---
+++ client/src/components/sidebar/sidebar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Sidebar extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Test</h1>
+
+            </div>
+        )
+    }
+}
+
+
+export default Sidebar;
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Sidebar from './components/sidebar/sidebar';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Sidebar />, document.getElementById('root'));
 registerServiceWorker();
